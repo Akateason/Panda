338803da782cec295b48dbcd0fa4e77af27c9427
akateason <xietianchen@subaojaojiang.com>
2016-09-21 14:03:01 +0800

    fake UI

--- GroupBuying/VC+V/Features/FeaturesController.m
+++ GroupBuying/VC+V/Features/FeaturesController.m
@@ -8,6 +8,30 @@
 
 #import "FeaturesController.h"
 
+@interface FeaturesController ()
+
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollview;
+@property (nonatomic,strong) UIImageView *imgView ;
+
+@end
+
 @implementation FeaturesController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad] ;
+    
+    
+    UIImage *image = [UIImage imageNamed:@"test_a"] ;
+    float imgWid = image.size.width ;
+    float imgHeight = image.size.height ;
+    CGRect rect = CGRectZero ;
+    rect.size = CGSizeMake(APP_WIDTH, APP_WIDTH * imgHeight / imgWid) ;
+    _imgView = [[UIImageView alloc] initWithFrame:rect] ;
+    _imgView.image = image ;
+    [_scrollview addSubview:_imgView] ;
+    _scrollview.contentSize = _imgView.frame.size ;
+}
+
 
 @end
--- GroupBuying/VC+V/Mine/MineController.m
+++ GroupBuying/VC+V/Mine/MineController.m
@@ -9,11 +9,29 @@
 #import "MineController.h"
 #import "UserOnDevice.h"
 
+
+@interface MineController ()
+
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollview;
+@property (nonatomic,strong) UIImageView *imgView ;
+
+@end
+
 @implementation MineController
 
 - (void)viewDidLoad
 {
     [super viewDidLoad] ;
+    
+    UIImage *image = [UIImage imageNamed:@"test_m"] ;
+    float imgWid = image.size.width ;
+    float imgHeight = image.size.height ;
+    CGRect rect = CGRectZero ;
+    rect.size = CGSizeMake(APP_WIDTH, APP_WIDTH * imgHeight / imgWid) ;
+    _imgView = [[UIImageView alloc] initWithFrame:rect] ;
+    _imgView.image = image ;
+    [_scrollview addSubview:_imgView] ;
+    _scrollview.contentSize = _imgView.frame.size ;
 }
 
 - (void)viewWillAppear:(BOOL)animated
